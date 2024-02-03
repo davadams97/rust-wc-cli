c409e605e50b6d6064175e568c99d43c25c1c4c7
David A. Adams <david@stemble.ca>
2024-02-03 01:11:35 -0500

    feat: output total lines in file

--- daawc/src/main.rs
+++ daawc/src/main.rs
@@ -1,20 +1,37 @@
 use clap::Parser;
 use anyhow::{Context, Result};
 use std::fs;
+use std::io::{BufRead, BufReader};
+use std::fs::File;
 
 #[derive(Parser)]
 struct Cli {    
     #[arg(short = 'c')]
+    count_bytes: bool,
+
+    #[arg(short = 'l')]
+    count_lines: bool,
+
     path: std::path::PathBuf,
 }
 
 fn main() -> Result<()>{
     let args = Cli::parse();
 
-    let file_content = fs::read(&args.path).with_context(|| format!("could not read file `{}`", args.path.display()))?;
-    let total_bytes = file_content.len();
+    if args.count_bytes {
+        let file_content = fs::read(&args.path).with_context(|| format!("could not read file `{}`", args.path.display()))?;
+        let total_bytes = file_content.len();
+        println!("Total bytes: {}", total_bytes);
+    } else if args.count_lines {
+        let file = File::open(&args.path)?;
+        let reader = BufReader::new(file);
+        let mut total_lines = 0;
 
-    println!("Total bytes: {}", total_bytes);
+        for _ in reader.lines() {
+            total_lines += 1;
+        }
+        println!("Total lines: {}", total_lines);
+    }
 
     Ok(())
 }
